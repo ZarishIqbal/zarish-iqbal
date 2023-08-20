2564b9d308ef94d81abd3957758622d470bb19be
zarishiqbal <zarishiqbal.zi@gmail.com>
2023-08-20 12:23:46 +0300

    configure routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ErrorBoundary } from "./components/error/ErrorBoundary";
+import "./index.css";
+import { Connect } from "./pages/Connect";
+import { Home } from "./pages/Home";
+import { Resume } from "./pages/Resume";
+import { TodayILearn } from "./pages/TodayILearn";
+import { Work } from "./pages/Work";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <ErrorBoundary />,
+  },
+  { path: "/home", element: <Home /> },
+  { path: "/work", element: <Work /> },
+  { path: "/resume", element: <Resume /> },
+  { path: "/today-i-learn", element: <TodayILearn /> },
+
+  { path: "/connect", element: <Connect /> },
+]);
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
